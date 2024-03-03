2e812e39946e9e57058254af12eccaa98800d04f
Nikita Petrovskiy <nikita.petrovskiy@wella.com>
2024-03-03 11:39:43 +0100

    section-footer creation

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const ctx = document.getElementById('myChart');
     data: {
       labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
       datasets: [{
-        label: 'Amount',
         data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28,25.48],
         borderWidth: 1
       }]
@@ -15,6 +14,9 @@ const ctx = document.getElementById('myChart');
         y: {
           beginAtZero: true
         }
+      },
+      legend:{
+        display:false
       }
     }
   });

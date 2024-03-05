0699423c44a11fb1e9c46d56b19b4636257314e5
Nikita Petrovskiy <nikita.petrovskiy@wella.com>
2024-03-05 19:33:14 +0100

    Almost completed layout of chart

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ const ctx = document.getElementById('myChart');
       labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
       datasets: [{
         data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28,25.48],
-        borderWidth: 1
+        borderWidth: 0,
+        backgroundColor:['hsl(10, 79%, 65%)', 'hsl(10, 79%, 65%)', 'hsl(186, 34%, 60%)', 'hsl(10, 79%, 65%)', 'hsl(10, 79%, 65%)', 'hsl(10, 79%, 65%)', 'hsl(10, 79%, 65%)'],
+        hoverBackgroundColor:['hsl(10, 79%, 75%)', 'hsl(10, 79%, 75%)', 'hsl(186, 34%, 70%)', 'hsl(10, 79%, 75%)', 'hsl(10, 79%, 75%)', 'hsl(10, 79%, 75%)', 'hsl(10, 79%, 75%)'],
+
       }]
     },
     options: {
+      plugins: {
+        legend:{
+          display:false,
+        },
+      },
       scales: {
         y: {
-          beginAtZero: true
-        }
+          beginAtZero: true,
+          display:false,
+        },
       },
-      legend:{
-        display:false
-      }
     }
   });
